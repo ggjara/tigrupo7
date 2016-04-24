903848e2995253a8f4958ab4499fe04b6eacfc04
Gonzalo <jara91@gmail.com>
2016-04-24 19:44:47 -0300

    Orden Metodo Parseo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-require 'base64'
-require 'cgi'
-require 'openssl'
-require 'hmac-sha1'
+
 
 key = '1234'
 signature = 'abcdef'
@@ -21,6 +18,10 @@ signature = 'abcdef'
 
 
   def hmac_sha1(data, secret)
+  	require 'base64'
+	require 'cgi'
+	require 'openssl'
+	require 'hmac-sha1'
 	hmac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), secret.encode("ASCII"), data.encode("ASCII"))
 	signature = Base64.encode64(hmac).chomp
 	return signature
