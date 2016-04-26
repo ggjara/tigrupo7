00a479ef57e7c2e977370b5d0e1219f970cf8155
Gonzalo <jara91@gmail.com>
2016-04-26 17:54:35 -0300

    Hotfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ def index
   render json: {Bienvenida: 'Bienvenido a la Home de nuestra pagina'}
 end
 
-#Metodo que Realiza una request.
+#Metodo que Realiza una request y retorna el body de la respuesta Parseado
 def requestWeb(typeOfRequest, uri, *paramsRequest)
   authKey = generateAuthToken(typeOfRequest, *paramsRequest)
   headers = { "Content-Type"=> "application/json", "Authorization"=> authKey} 
@@ -17,8 +17,12 @@ def requestWeb(typeOfRequest, uri, *paramsRequest)
   paramsRequest.each do |param|
    query.store(param.name, param.value)
   end
+  if typeOfRequest=='GET'
+    response =HTTParty.get(uri, :query => query, :headers => headers)
+  elsif typeOfRequest=='POST'  
+    #Hacer respuesta para POST
+  end
   
-  response =HTTParty.get(uri, :query => query, :headers => headers)
   return JSON.parse(response.body)   
 end
 
@@ -44,9 +48,73 @@ def hmac_sha1(data, secret)
   return signature
 end
 
-#Genera una Instancia de Param
+#Retorna una Instancia de Param con los params 'name' y 'value'
 def generateParam(name, value)
   return Param.new(name: name, value: value)
 end
+
+#Inicia Bodega. Retorna Bodega con 'name': 'grupo7'.
+def iniciarBodega
+  Bodega.destroy_all
+  Almacen.destroy_all
+  Producto.destroy_all
+  bodegaGrupo7 = Bodega.new(name: 'grupo7')
+  bodegaGrupo7.save
+  getAlmacenes(true)
+  return bodegaGrupo7
+end
+
+def getAlmacenes(conCreacion)
+  if(conCreacion)
+    bodegaGrupo7 = Bodega.find_by name: 'grupo7'
+    jsonAlmacenes = requestWeb('GET', 'http://integracion-2016-dev.herokuapp.com/bodega/almacenes')
+    jsonAlmacenes.each do |jsonAlmacen|
+        almacenCreado = Almacen.new(
+          bodega: bodegaGrupo7, 
+          _id: jsonAlmacen['_id'], 
+          grupo: jsonAlmacen['grupo'], 
+          pulmon: jsonAlmacen['pulmon'], 
+          despacho: jsonAlmacen['despacho'], 
+          recepcion: jsonAlmacen['recepcion'], 
+          totalSpace: jsonAlmacen['totalSpace'], 
+          usedSpace: jsonAlmacen['usedSpace'])
+        almacenCreado.save
+        getSkusWithStock(almacenCreado, conCreacion)
+    end 
+    else
+  end
+end
+
+def getSkusWithStock(almacen, conCreacion)
+  if(conCreacion)
+    jsonSkusWithStock = requestWeb('GET', 'http://integracion-2016-dev.herokuapp.com/bodega/skusWithStock', 
+      generateParam('almacenId',almacen._id))
+    jsonSkusWithStock.each do |jsonSku|
+      skuFinded = jsonSku['_id']
+      getStock(almacen, skuFinded, conCreacion)
+    end
+  else
+  end
+end
+
+def getStock(almacen, sku, conCreacion)
+  if(conCreacion)
+    jsonProducts = requestWeb('GET', 'http://integracion-2016-dev.herokuapp.com/bodega/stock', 
+      generateParam('almacenId', almacen._id), generateParam('sku', sku))
+    jsonProducts.each do |jsonProduct|
+      productoCreado = Producto.new(
+        almacen: almacen, 
+        _id: jsonProduct['_id'].to_s, 
+        sku: jsonProduct['sku'], 
+        costo: jsonProduct['costo'], 
+        despachado: jsonProduct['despachado'], 
+        precio: jsonProduct['precio'], 
+        grupo: jsonProduct['grupo'])
+      productoCreado.save
+    end
+  else
+  end 
+end
+
 
 end
--- app/controllers/bodegas_controller.rb
+++ app/controllers/bodegas_controller.rb
@@ -3,62 +3,7 @@ class BodegasController < ApplicationController
 #generateParam(name, value)
 
 def consultarInfo
-	render json: {NBodegas: @bodegas}
+	render json: Bodega.first
 end
-
-
-def crearInfo
-	Bodega.destroy_all
-	Almacen.destroy_all
-	Producto.destroy_all
-	bodegaGrupo7 = Bodega.new(name: 'grupo7')
-	bodegaGrupo7.save
-	getAlmacenes(true)
-	render json: {NAlmacenes: bodegaGrupo7.cantAlmacenes, Nproductos: bodegaGrupo7.cantProductos}
-end
-
-def getAlmacenes(conCreacion)
-	if(conCreacion)
-		bodegaGrupo7 = Bodega.find_by name: 'grupo7'
-		jsonAlmacenes = requestWeb('GET', 'http://integracion-2016-dev.herokuapp.com/bodega/almacenes')
-		jsonAlmacenes.each do |jsonAlmacen|
-		    almacenCreado = Almacen.new(bodega: bodegaGrupo7, _id: jsonAlmacen['_id'], grupo: jsonAlmacen['grupo'], 
-		    	pulmon: jsonAlmacen['pulmon'], despacho: jsonAlmacen['despacho'], recepcion: jsonAlmacen['recepcion'], 
-		    	totalSpace: jsonAlmacen['totalSpace'], usedSpace: jsonAlmacen['usedSpace'])
-		    almacenCreado.save
-		    getSkusWithStock(almacenCreado, conCreacion)
-		end	
-  	else
-	end
-end
-
-def getSkusWithStock(almacen, conCreacion)
-	if(conCreacion)
-		jsonSkusWithStock = requestWeb('GET', 'http://integracion-2016-dev.herokuapp.com/bodega/skusWithStock', 
-			generateParam('almacenId',almacen._id))
-		jsonSkusWithStock.each do |jsonSku|
-			skuFinded = jsonSku['_id']
-			getStock(almacen, skuFinded, conCreacion)
-		end
-	else
-	end
-end
-
-def getStock(almacen, sku, conCreacion)
-	if(conCreacion)
-		jsonProducts = requestWeb('GET', 'http://integracion-2016-dev.herokuapp.com/bodega/stock', 
-			generateParam('almacenId', almacen._id), generateParam('sku', sku))
-		jsonProducts.each do |jsonProduct|
-			productoCreado = Producto.new(almacen: almacen, 
-				_id: jsonProduct['_id'].to_s, 
-				sku: jsonProduct['sku'], 
-				costo: jsonProduct['costo'], despachado: jsonProduct['despachado'], precio: jsonProduct['precio'], 
-				grupo: jsonProduct['grupo'])
-			productoCreado.save
-		end
-	else
-	end	
-end
-
 
 end
