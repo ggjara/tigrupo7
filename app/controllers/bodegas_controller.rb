064f8798aec46bddeee180c6453a8ef759fe12e6
Jesu <mjrivera2@puc.cl>
2016-04-27 15:15:27 -0300

    add Bodegas controller, view, etc

--- app/controllers/bodegas_controller.rb
+++
@@ -1,9 +0,0 @@
-class BodegasController < ApplicationController
-#requestWeb(typeOfRequest, uri, *paramsRequest)
-#generateParam(name, value)
-
-def consultarInfo
-	render json: Bodega.first
-end
-
-end
---
+++ test/controllers/bodegas_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BodegasControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
