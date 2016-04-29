67ae1a781b40a5a204d15b2c15719dde667489c2
Gonzalo <jara91@gmail.com>
2016-04-29 18:06:57 -0300

    Hotfix Schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,8 @@
 #Para probar en ambiente de desarrollo 
-
 #ENV['RAILS_ENV'] = "development"
 #set :environment, 'development'
+
 set :output, "#{path}/log/cron_log.log"
-
-
 
 every 2.minutes do
   runner "Bodega.cambiar"
