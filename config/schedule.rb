d8f763632d867c79f24019900bc289e60e905d0e
Gonzalo <jara91@gmail.com>
2016-05-02 11:09:37 -0300

    CronWhenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,11 +2,6 @@
 #ENV['RAILS_ENV'] = "development"
 #set :environment, 'development'
 
-env :PATH, ENV['PATH']
-env :GEM_PATH, ENV['GEM_PATH']
-
-set :bundle_command, "/bin/bundle"
-set :whenever_command, "/bin/whenever"
 
 set :output, "#{path}/log/cron_log.log"
 
