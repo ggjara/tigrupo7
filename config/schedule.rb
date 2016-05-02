8889f69f07257c8397d0b896aafea72376f5ce37
Gonzalo <jara91@gmail.com>
2016-05-02 11:45:58 -0300

    Cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@
 
 set :output, "#{path}/log/cron_log.log"
 
-job_type :runner, "cd #{path} && RAILS_ENV=production /home/deploy/.rvm/wrappers/ruby 2.3.0@default/bundle exec rails runner ':task' :output"
+job_type :runner, "cd #{path} && RAILS_ENV=production /home/deploy/.rvm/wrappers/ruby-2.3.0@global/bundle exec rails runner ':task' :output"
 
 
 every 2.minutes do
