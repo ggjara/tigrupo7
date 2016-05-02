b79ab6b4d49ff1246d7b000ad51da4172cf0ce61
Gonzalo <jara91@gmail.com>
2016-05-02 16:30:37 -0300

    Modificado Link Dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ set :deploy_to, '/home/deploy/tigrupo7'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 
 namespace :deploy do
