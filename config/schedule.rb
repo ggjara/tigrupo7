fdafba42787f5112666dfbe41e97dd1665da400e
Gonzalo <jara91@gmail.com>
2016-05-02 11:32:50 -0300

    runner taks

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,9 @@
 
 set :output, "#{path}/log/cron_log.log"
 
+job_type :runner, "cd #{path} && RAILS_ENV=production /home/deploy/.rvm/wrappers/ruby 2.3.0p0@rails-4.2/bundle exec rails runner ':task' :output"
+
+
 every 2.minutes do
   runner "Bodega.cambiar"
 end
