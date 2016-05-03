68d78fb39a940ce4e68d726643f2314b2c98dd67
Gonzalo <jara91@gmail.com>
2016-05-02 22:29:36 -0300

    Agregado despacho Productos

--- app/models/almacen.rb
+++ app/models/almacen.rb
@@ -3,11 +3,29 @@ class Almacen < ActiveRecord::Base
 	has_many :productos, dependent: :destroy
 
 
+def tieneEspacio(sku)
+	if (self.totalSpace - self.usedSpace)> espacio(sku)
+		return true
+	else
+		return false
+	end
+end
+
 def tieneProducto(sku)
 	if self.productos.where(sku: sku).count>0
-			return true
+		return true
 	end
 	return false
+end
+
+def eliminarEspacio
+	self.usedSpace = self.usedSpace - 1
+	self.save
+end
+
+
+def espacio(sku)		
+	return 1
 end
 
 end
