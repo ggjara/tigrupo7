fb3f24dd2ad0387fe68cde40b15c8d46917c7f73
Jesu <mjrivera2@puc.cl>
2016-06-27 18:04:11 -0300

    Arreglo consulta bi financiero

--- app/controllers/bi_financieros_controller.rb
+++ app/controllers/bi_financieros_controller.rb
@@ -1,7 +1,15 @@
 class BiFinancierosController < ApplicationController
   def index
+    @facturas = Factura.all.where('created_at > ?', 7.days.ago)
+    @boletas = Bill.all.where('created_at > ?', 7.days.ago)
+    @facturasFTP = @facturas.where(cliente: "internacional")
+    @facturasB2Be = @facturas.where.not(cliente: "internacional", cliente: Cliente.find_by(grupo: 7)._idGrupo)
+    @facturasB2Br = @facturas.where.not(cliente: "internacional", proveedor: Cliente.find_by(grupo: 7)._idGrupo)
+    @saldos = Infosaldo.order(:fecha)
+
   end
 
   def show
+    @transacciones = Trx.where(fechaCreacion: DateTime.parse(params[:id]).beginning_of_day..DateTime.parse(params[:id]).end_of_day)
   end
 end
