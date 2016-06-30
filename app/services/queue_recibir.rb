1d6ad46e8dd92b54de73b409b554e05425268857
Gonzalo <jara91@gmail.com>
2016-06-30 00:53:12 -0400

    Probando mensajes

--- app/services/queue_recibir.rb
+++ app/services/queue_recibir.rb
@@ -37,26 +37,28 @@ def threadReceive
       #Crear promoción
       ap = AppPromotion.create(sku: sku.to_s, precio: precio.to_i, fechaInicio: inicio, fechaTermino: fin, codigo: codigo.to_s)
       #Crear publicación
-      if(publicar)
-        producto = ""
-        url_imagen = ""
-        if(ap.sku=='1')
-          producto = "Pollo"
-          url_imagen = "http://pollopepe.com.mx/wp-content/uploads/2013/07/01-pollo-entero1.jpg"
-        elsif (ap.sku=='10')
-          producto = "Pan Marraqueta"
-          url_imagen = "http://www.cl.all.biz/img/cl/catalog/37676.jpeg"
-        elsif (ap.sku=='23')
-          producto = "Harina"
-          url_imagen = "http://2.bp.blogspot.com/-R_vACaZLlIU/VMEdsXeWeOI/AAAAAAAAAQk/oSbk3LTj3GA/s900/harina.PNG"
-        elsif (ap.sku=='39')
-          producto = "Uva"
-          url_imagen = "http://difundir.org/wp-content/uploads/2015/04/hu2.jpg"
-        end
+      if(sku.to_s=='1' || sku.to_s=='10' || sku.to_s =='23' || sku.to_s == '39')
+        if(publicar)
+          producto = ""
+          url_imagen = ""
+          if(ap.sku=='1')
+            producto = "Pollo"
+            url_imagen = "http://pollopepe.com.mx/wp-content/uploads/2013/07/01-pollo-entero1.jpg"
+          elsif (ap.sku=='10')
+            producto = "Pan Marraqueta"
+            url_imagen = "http://www.cl.all.biz/img/cl/catalog/37676.jpeg"
+          elsif (ap.sku=='23')
+            producto = "Harina"
+            url_imagen = "http://2.bp.blogspot.com/-R_vACaZLlIU/VMEdsXeWeOI/AAAAAAAAAQk/oSbk3LTj3GA/s900/harina.PNG"
+          elsif (ap.sku=='39')
+            producto = "Uva"
+            url_imagen = "http://difundir.org/wp-content/uploads/2015/04/hu2.jpg"
+          end
 
-        mensajeAPublicar = "¡Nueva Promoción! - "<<producto<< " a sólo: $ "<<precio.to_s<<" - Entre las fechas: "<<ap.fechaInicio.day.to_s<<"/"<<ap.fechaInicio.month.to_s<<"/"<<ap.fechaInicio.year.to_s<<"-"<<ap.fechaTermino.day.to_s<<"/"<<ap.fechaTermino.month.to_s<<"/"<<ap.fechaTermino.year.to_s<<" CODIGO #"<<codigo.to_s<<"."
-        puts mensajeAPublicar
-        Bodega.publish({message: mensajeAPublicar, media: url_imagen})
+          mensajeAPublicar = "¡Nueva Promoción! - "<<producto<< " a sólo: $ "<<precio.to_s<<" - Entre las fechas: "<<ap.fechaInicio.day.to_s<<"/"<<ap.fechaInicio.month.to_s<<"/"<<ap.fechaInicio.year.to_s<<"-"<<ap.fechaTermino.day.to_s<<"/"<<ap.fechaTermino.month.to_s<<"/"<<ap.fechaTermino.year.to_s<<" CODIGO #"<<codigo.to_s<<"."
+          puts mensajeAPublicar
+          Bodega.publish({message: mensajeAPublicar, media: url_imagen})
+        end
       end
 
 
@@ -99,13 +101,47 @@ def send
   e = ch.exchange("")
 
   paramsMsg = '{ "sku": 1,
-    "precio": 10,
-    "inicio": 1467158400,
-    "fin": 1468972800,
-    "codigo": 123,
+    "precio": 1000,
+    "inicio": 1467086400000,
+    "fin": 1469678400000,
+    "codigo": promoPollo,
     "publicar": true}'
   # publish a message to the exchange which then gets routed to the queue
   e.publish(paramsMsg, :key => 'ofertas')
+
+  # sleep 3
+
+  # paramsMsg2 = '{ "sku": 10,
+  #   "precio": 15000,
+  #   "inicio": 1467086400000,
+  #   "fin": 1469678400000,
+  #   "codigo": promoPan,
+  #   "publicar": true}'
+  # # publish a message to the exchange which then gets routed to the queue
+  # e.publish(paramsMsg2, :key => 'ofertas')
+
+  # sleep 3
+
+  #  paramsMsg3 = '{ "sku": 23,
+  #   "precio": 4000,
+  #   "inicio": 1467086400000,
+  #   "fin": 1469678400000,
+  #   "codigo": promoHarina,
+  #   "publicar": true}'
+  # # publish a message to the exchange which then gets routed to the queue
+  # e.publish(paramsMsg3, :key => 'ofertas')
+
+  # sleep 3
+
+  #   paramsMsg4 = '{ "sku": 23,
+  #   "precio": 1000,
+  #   "inicio": 1467086400000,
+  #   "fin": 1469678400000,
+  #   "codigo": promoUva,
+  #   "publicar": true}'
+  # # publish a message to the exchange which then gets routed to the queue
+  # e.publish(paramsMsg4, :key => 'ofertas')
+
 
 
   b.stop # close the connection
